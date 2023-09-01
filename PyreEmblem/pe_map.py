5d23a09d4313b0736fd0aac8b76adb8958e04f20
EmilJose-towson <ejose1@students.towson.edu>
2023-09-01 13:23:17 -0400

    Update pe_map.py

--- PyreEmblem/pe_map.py
+++ PyreEmblem/pe_map.py
@@ -13,6 +13,13 @@ def set_cols(num):
 def set_rows(num):
     global rows; rows = num
 
+# get global cols var
+def get_cols():
+    global cols; return cols
+
+def get_rows():
+    global rows; return rows
+
 # return the zeroed array result of size c by r
 def gen_map(c, r):
     global map_arr
@@ -20,6 +27,9 @@ def gen_map(c, r):
     set_rows(r)
     result = [[0 for i in range(c)] for j in range(r)]
     map_arr = result
+
+def get_map():
+    global map_arr; return map_arr
 
 # print map_arr
 def print_map():
@@ -46,3 +56,15 @@ def print_map():
     for i in range(0, w):
         print('--', end=' ')
     print('-')
+
+def load_map(map_preset):
+    # access current map
+    global map_arr
+    # get dimensions of map to load
+    w, h = (len(map_preset[0]), len(map_preset))
+    # reset current map
+    gen_map(w, h)
+    # copy each tile from loaded map to map_arr
+    for i in range(0, h):
+        for j in range(0, w):
+            map_arr[i][j] = map_preset[i][j]
