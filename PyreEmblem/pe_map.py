fb422e097bb7dc8ac89b3f70436be4c87bb4bd29
Emilson-Jose <emilson.h.jose@gmail.com>
2023-09-11 00:14:10 -0400

    Update pe_map.py
    Define initial tile set in pe_map.py

--- PyreEmblem/pe_map.py
+++ PyreEmblem/pe_map.py
@@ -3,7 +3,22 @@ import random as rand
 global rows; rows = 3
 global cols; cols = 3
 global map_arr; map_arr = [[0 for i in range(cols)] for j in range(rows)]
-
+# tile set below is defined as follows
+#   each row is a tile definition
+#   col 1 = tile name
+#   col 2 = evasion bonus
+#   col 3 = defense bonus
+#   col 4 = resistance bonus
+#   col 5 = heal per turn
+#   col 6 = movement cost
+global tile_set; tile_set = [['[]', 0, 0, 0, 0, 0]      # floor
+                             ['TR', 5, 1, 0, 0, 1]      # trees
+                             ['MT', 20, 1, 1, 0, 4]     # mountain
+                             ['FT', 12, 2, 2, 0, 0]     # fort
+                             ['PK', 35, 1, 0, 0, 6]     # mountain peak
+                             ['TH', 12, 2, 4, 4, 0]     # throne
+                             ['CH', 10, 0, 1, 10, 0]    # church
+                             ['WA', 0, 0, 0, 0, 99]]    # wall
 
 # set global cols var
 def set_cols(num):
