daf5164f9e0d9dcc2f2e7fe566c820a46a77a9df
Emilson-Jose <emilson.h.jose@gmail.com>
2023-09-12 12:20:31 -0400

    Update my_obj.py
    Removed print_2darray and global test variables
    
    Initialized pe_map map with 9x9 0's array before printing
    
    Also updated "made with" to reflect python 3.11

--- PyreEmblem/my_obj.py
+++ PyreEmblem/my_obj.py
@@ -1,12 +1,10 @@
-# made with Python 3.10.11
+# made with Python 3.11.5
 
 import random as rand
 import subprocess as subp
 import os
 import pe_map
 
-global var1; var1 = 0
-global momba; momba = "Monday"
 
 def hit_calc(a_spd, a_hit, d_avd):
     result = 0
@@ -18,29 +16,6 @@ def rn_check(hit):
         return True
     else:
         return False
-    
-def print_2darray(a):
-    w, h = (len(a[0]), len(a))
-    #print(flush=True)          # flush console
-    # print top border
-    print('-', end=' ')
-    for i in range(0, w):
-        print('--', end=' ')
-    print('-')
-    # print array with leading + ending borders
-    for i in range(0, h):
-        print('|', end=' ')
-        for j in range(0, w):
-            if a[i][j] < 10:
-                print("0" + str(a[i][j]), end=' ')
-            else:
-                print(a[i][j], end=' ')
-        print('|')
-    # print bottom border
-    print('-', end=' ')
-    for i in range(0, w):
-        print('--', end=' ')
-    print('-')
 
 # sets tiles in an array
 def gen_map(map):
@@ -55,11 +30,11 @@ def gen_map(map):
 def main():
     rows, cols = (9, 9)
     arr = [[0 for i in range(cols)] for j in range(rows)]
-    print_2darray(arr)
     print('===================================================')
     #os.system('cls')
     arr = gen_map(arr)
     #print_2darray(arr)
+    pe_map.load_map(arr)
     pe_map.print_map()
     print('===================================================')
     arr = [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
