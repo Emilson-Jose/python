ae1409eb438c1317bc48bf5a363a83edc2db5603
Emilson-Jose <emilson.h.jose@gmail.com>
2023-12-19 15:06:39 -0500

    Update test.py

--- pract/test.py
+++ pract/test.py
@@ -1,8 +1,11 @@
 def main():
-    my_list = [1, 2, 3, 4, 5, 'a', 'b', 'c']
+    my_list = [1, 2, 3, 4, 5, 6, 7, 8]
     other_list = my_list[:4]
     print(other_list)
-    print(my_list.sort())
+    print([i for i in my_list if (i%2)==0])
+    x = 1
+    y = 2
+    print(x, "%", y, "=", x%y)
 
 if __name__ == "__main__":
     main()
